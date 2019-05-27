ec3ada3b823f9eabd25d5db3d199e96ddcd6e713
Nicholas <nicholasnavarro98@gmail.com>
2019-05-27 19:09:47 +0300

    fixed error 'str attribute assigned no value' by adding .close() instead of .close. furthermore, added an option to close files by using raw input

--- ex15.py
+++ ex15.py
@@ -4,14 +4,26 @@ script, filename = argv
 
 txt = open(filename)
 
+
 print "Here's your file %r:" % filename 
 print txt.read()
+txtclose = txt.close()
 
 print "Type the filename again:"
 file_again = raw_input("> ")
-
 txt_again = open(file_again)
 
 print txt_again.read()
+txt_again_close = txt_again.close()
+
+print "would you like to close this file? Y/N"
+fileClose = raw_input('> ')
+if fileClose == 'Y':
+    txtclose
+    txt_again_close
+else:
+    print "It's important to close files once you're done with them"
+
+
 
 
