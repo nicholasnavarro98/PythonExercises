a658d8c8b8d8571c85f567656b2ab1c1984461f2
Nicholas Navarro <nicholasnavarro98@gmail.com--global user.name NicholasMacbookgit statusgit config --global user.email nicholasnavarro98@gmail.com>
2019-06-04 12:13:10 +0300

    added all necessary comments to script

--- ex16.py
+++ ex16.py
@@ -8,27 +8,27 @@ print "If you do want that, hit RETURN." #prints line telling user to hit enter 
 
 raw_input("?") #detecs user input with ? as prompt
 
-print "Opening the file..."
-target = open(filename, 'w')
+print "Opening the file..." #tells the user that opening the file
+target = open(filename, 'w') #open(filename) is the file, and its stored in the 'target' variable
 
-print "Truncating the file. Goodbye!"
-target.truncate
+print "Truncating the file. Goodbye!" #telling user that file is Truncating
+target.truncate #target file has been truncated
 
-print "Now I'm going to ask you for three lines"
+print "Now I'm going to ask you for three lines" #message to tell user to enter three lines
 
-line1 = raw_input("line1:")
+line1 = raw_input("line1:") # three lines that prompts the user to enter a string	
 line2 = raw_input("line2:")
 line3 = raw_input("line3:")
 
-print "I'm going to write these to the file."
+print "I'm going to write these to the file." # tells user that its writing to file
 
-target.write(line1)
+target.write(line1) #target. write = writing the lines to 'target'
 target.write("\n")
 target.write(line2)
 target.write("\n")
 target.write(line3)
 target.write("\n")
 
-print "And now, we will close the file"
+print "And now, we will close the file" #tells user target is closing
 
-target.close()
+target.close() # 'target' is closed using .close method
