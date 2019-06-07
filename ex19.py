1988ba90bf629b6d8da2b992432c55f1cd915e4d
Nicholas Navarro <nicholasnavarro98@gmail.com--global user.name NicholasMacbookgit statusgit config --global user.email nicholasnavarro98@gmail.com>
2019-06-07 12:48:46 +0300

    Added exercise 19, more on functions

---
+++ ex19.py
@@ -0,0 +1,16 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print "You have %s cheeses!" % cheese_count
+	print "You have %d boxes_of_crackers!" % boxes_of_crackers
+	print "Man thats enough for a party!"
+	print "Get a blanket. \n"
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20,30)
+
+
+print "OR, we can use variables"
+cheese = 10
+crackers = 20
+
+cheese_and_crackers(cheese,crackers)
+
